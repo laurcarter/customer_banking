75172dc5313a6c0f1bcba37cb22724266cbe21ef
laurcarter <62532395+laurcarter@users.noreply.github.com>
2024-07-29 19:40:39 -0400

    Fixed code based on TA help

--- M3_Starter_Code/customer_banking.py
+++ M3_Starter_Code/customer_banking.py
@@ -18,7 +18,7 @@ def main():
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Interest earned: {interest_earned:.2f}")
+    print(f"Interest earned in {savings_maturity} months: {interest_earned:.2f}")
     print(f"Updated savings account balance: {updated_savings_balance:.2f}")
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
@@ -30,7 +30,7 @@ def main():
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Interest earned: {interest_earned:.2f}")
+    print(f"Interest earned in {cd_maturity} months: {interest_earned:.2f}")
     print(f"Updated CD account balance: {updated_cd_balance:.2f}")
 if __name__ == "__main__":
     # Call the main function.
